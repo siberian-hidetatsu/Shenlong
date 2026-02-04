2a203d213b351f26752289a31e64961d0097e17b
Hidetatsu Miyamoto <siberian.hidetatsu@gmail.com>
2026-02-04 10:02:53 +0900

    version up

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("2.7.*")]
+[assembly: AssemblyVersion("2.8.*")]
